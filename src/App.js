6befd11c5ba20665bb631f7a5386805fa294d184
Inna Botchkina <innabobolina@yahoo.com>
2022-01-13 18:18:02 -0500

    create inputTextHandler and submitTodoHandler functions

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState('');
-  
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>Inna's Todo List</h1>
       </header>
       <div>
-        <Form />
+        <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
+        <h1>{inputText}</h1>
         <TodoList />
       </div>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Form = () => {
+const Form = ({inputText, setInputText, todos, setTodos}) => {
+    
+    const inputTextHandler = event => {
+        console.log(event.target.value);
+        setInputText(event.target.value);
+    }
+
+    const submitTodoHandler = event => {
+        event.preventDefault();
+        console.log(event);
+        setTodos([...todos, {text: inputText, completed: false, id: Math.random()}])
+        setInputText("");
+    }
+
     return (
-        <form>
-            <input type="text" className="todo-input"/>
+        <form onSubmit={submitTodoHandler}>
+            <input onChange={inputTextHandler} 
+            value={inputText}
+                type="text" className="todo-input"/>
             <button type="submit" className="todo-button">
-            <i class="fas fa-plus-square"></i>
+                <i class="fas fa-plus-square"></i>
             </button>
             <div className="todo-select">
                 <select name="todos" id="" className="select-todos">
